11679c1fae8c8063bcaba07d7512f34fcacbef58
Saidali Saburov <saidalisaburov@gmal.com>
2024-06-08 13:38:45 +0500

    -init

---
+++ src/test/java/tj/sidalsoft/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package tj.sidalsoft.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
